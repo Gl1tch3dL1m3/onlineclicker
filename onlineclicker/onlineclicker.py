7766e945ec92bc5d91b56d4b240595110443e0da
glitchedlime <gabcosamuel8@gmail.com>
2026-08-05 21:53:35 +0200

    update v1.1

--- onlineclicker/onlineclicker.py
+++ onlineclicker/onlineclicker.py
@@ -78,7 +78,7 @@ def _get_ini_value(section, value, _type=None):
 
 _DB_TYPE = _get_ini_value("Global", "DB_TYPE") if _get_ini_value("Global", "DB_TYPE") != None else "SQLite3"
 _PLAYERS_COLUMN = "test_players" if _testing else "players" # don't mind this - testing purposes
-_CURRENT_VERSION = "1.6.1" # this is a mod client version
+_CURRENT_VERSION = "1.6" # this is a mod client version
 _SERVER_VERSION = "1.1"
 _PORT = _get_ini_value("Server", "PORT", int) if _get_ini_value("Server", "PORT", int) != None else 24588
 _SERVER_NAME = _get_ini_value("Server", "SERVER_NAME") if _get_ini_value("Server", "SERVER_NAME") != None else "OnlineClicker Server"
